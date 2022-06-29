b62321c87953e167b1787c54aa4f28f8dab59ac1
IntentG <sublustrum123@gmail.com>
2022-06-30 00:01:18 +0600

    Этап 2. Добавление метода вычисления необходимых значений

--- Program.cs
+++ Program.cs
@@ -9,11 +9,35 @@ int countArrayValue = int.Parse(Console.ReadLine());
 string [] startArray = new string [countArrayValue];
  
  string [] FillArray (string [] array)
-{
+ {
 	for (int i = 0; i < countArrayValue; i++)
 	{
 		Console.WriteLine($"Введите {i} значение:");
 		array[i] = Console.ReadLine();
 	}
 	return array;
+}
+
+string [] SortArray (string [] array)
+{
+	int countNecessaryValue = 0;
+	string arrayValue = string.Empty;
+	for (int i = 0; i < array.Length; i++)
+	{
+		arrayValue = array[i];
+		if (arrayValue.Length <= 3) countNecessaryValue++;
+	}
+
+	string [] endArray = new string [countNecessaryValue];
+	int index = 0;
+	for (int i = 0; i < array.Length; i++)
+	{
+		arrayValue = array[i];
+		if (arrayValue.Length <= 3)
+		{
+			endArray[index] = array[i];
+			index++;
+		}
+	}
+	return endArray;
 }
