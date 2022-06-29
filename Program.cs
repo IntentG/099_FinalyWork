01b7849f89723e939ee4d6061713578d0c200d4b
IntentG <sublustrum123@gmail.com>
2022-06-29 23:55:50 +0600

    Этап 1.1 Заполнение массива данными пользователя

--- Program.cs
+++ Program.cs
@@ -8,3 +8,12 @@ int countArrayValue = int.Parse(Console.ReadLine());
 
 string [] startArray = new string [countArrayValue];
  
+ string [] FillArray (string [] array)
+{
+	for (int i = 0; i < countArrayValue; i++)
+	{
+		Console.WriteLine($"Введите {i} значение:");
+		array[i] = Console.ReadLine();
+	}
+	return array;
+}
